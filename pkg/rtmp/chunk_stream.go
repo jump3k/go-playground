e1c9c80bf6aafcebcbfd09e29a5463d6755922ee
xly0713 <xly80713@gmail.com>
2021-02-01 17:44:07 +0800

    code refinement

--- pkg/rtmp/chunk_stream.go
+++ pkg/rtmp/chunk_stream.go
@@ -31,7 +31,6 @@ type ChunkStream struct {
 	ChunkHeader
 	ChunkBody []byte
 
-	tmpFormat    uint8
 	msgHdrSize   int
 	timeExtended bool
 	gotBodyFull  bool
@@ -124,33 +123,6 @@ func (c *Conn) readChunkStream() (*ChunkStream, error) {
 			c.onReadChunkStreamSucc(cs)
 			return cs, nil
 		}
-
-		/*
-			h, err := c.ReadUint(1, true)
-			if err != nil {
-				return nil, err
-			}
-
-			fmt := h >> 6
-			csid := h & 0x3f
-			cs, ok := c.chunks[csid]
-			if !ok {
-				cs = newChunkStream()
-				c.chunks[csid] = cs
-			}
-
-			cs.tmpFormat = uint8(fmt)
-			cs.ChunkBasicHeader.Csid = csid
-			if err := c.readChunk(cs); err != nil {
-				return nil, err
-			}
-			//c.chunks[csid] = cs
-
-			if cs.gotBodyFull {
-				c.onReadChunkStreamSucc(cs)
-				return cs, nil
-			}
-		*/
 	}
 }
 
@@ -378,129 +350,6 @@ func (c *Conn) ack(size uint32) {
 	}
 }
 
-func (c *Conn) readChunk(cs *ChunkStream) error {
-	if cs.bodyRemain != 0 && cs.tmpFormat != 3 {
-		return fmt.Errorf("remain(%d) not zero while tmpFormat as 1/2/3", cs.bodyRemain)
-	}
-
-	switch cs.Csid {
-	case 0:
-		id, _ := c.ReadUint(1, false)
-		cs.Csid = id + 64
-	case 1:
-		id, _ := c.ReadUint(2, false)
-		cs.Csid = id + 64
-	}
-
-	setRemainFlag := func(cs *ChunkStream) {
-		cs.gotBodyFull = false
-		cs.bodyIndex = 0
-		cs.bodyRemain = cs.MsgLength
-		cs.ChunkBody = make([]byte, int(cs.MsgLength))
-	}
-
-	switch cs.tmpFormat {
-	case 0:
-		// message header need 11 bytes while fmt=0
-		cs.Fmt = 0
-		cs.TimeStamp, _ = c.ReadUint(3, true)
-		cs.MsgLength, _ = c.ReadUint(3, true)
-		msgTypeId, _ := c.ReadUint(1, true)
-		cs.MsgTypeID = RtmpMsgTypeID(msgTypeId)
-		cs.MsgStreamID, _ = c.ReadUint(4, false)
-
-		cs.timeExtended = false
-		if cs.TimeStamp == 0xffffff {
-			// if extended timestamp, read 4 bytes
-			cs.TimeStamp, _ = c.ReadUint(4, true)
-			cs.timeExtended = true
-		}
-		setRemainFlag(cs)
-	case 1:
-		// message header need 7 bytes while fmt=1
-		cs.Fmt = 1
-		timeStamp, _ := c.ReadUint(3, true)
-		cs.MsgLength, _ = c.ReadUint(3, true)
-		msgTypeId, _ := c.ReadUint(1, true)
-		cs.MsgTypeID = RtmpMsgTypeID(msgTypeId)
-
-		cs.timeExtended = false
-		if timeStamp == 0xffffff {
-			// if extended timestamp, read 4 bytes
-			timeStamp, _ = c.ReadUint(4, true)
-			cs.timeExtended = true
-		}
-
-		cs.ExtendedTimeStamp = timeStamp
-		cs.TimeStamp += timeStamp
-		setRemainFlag(cs)
-	case 2:
-		// message header need 3 bytes while fmt=2
-		cs.Fmt = 2
-		timeStamp, _ := c.ReadUint(3, true)
-
-		cs.timeExtended = false
-		if timeStamp == 0xffffff {
-			// if extended timestamp, read 4 bytes
-			timeStamp, _ = c.ReadUint(4, true)
-			cs.timeExtended = true
-		}
-
-		cs.ExtendedTimeStamp = timeStamp
-		cs.TimeStamp += timeStamp
-		setRemainFlag(cs)
-	case 3:
-		if cs.bodyRemain == 0 {
-			switch cs.Fmt {
-			case 0:
-				if cs.timeExtended {
-					cs.TimeStamp, _ = c.ReadUint(4, true)
-				}
-			case 1, 2:
-				timedelta := cs.ExtendedTimeStamp
-				if cs.timeExtended {
-					timedelta, _ = c.ReadUint(4, true)
-				}
-				cs.TimeStamp += timedelta
-			}
-			setRemainFlag(cs)
-		} else {
-			if cs.timeExtended {
-				b, err := c.readWriter.Peek(4)
-				if err != nil {
-					return err
-				}
-
-				tmpTimeStamp := binary.BigEndian.Uint32(b)
-				if tmpTimeStamp == cs.TimeStamp {
-					_, _ = c.readWriter.Discard(4)
-				}
-			}
-		}
-	default:
-		return fmt.Errorf("invalid rtmp format: %d", cs.Fmt)
-	}
-
-	size := cs.bodyRemain
-	if size > c.remoteChunkSize {
-		size = c.remoteChunkSize //important: read chunk from peer accord to min(remoteChunkSize, cs.remain)
-	}
-
-	buf := cs.ChunkBody[cs.bodyIndex : cs.bodyIndex+size]
-	if nr, err := c.readWriter.Read(buf); err != nil || nr != int(size) {
-		return errors.Wrapf(err, "read %d bytes, autual: %d", size, nr)
-	} else {
-		cs.bodyIndex += uint32(nr)
-		cs.bodyRemain -= uint32(nr)
-
-		if cs.bodyRemain == 0 {
-			cs.gotBodyFull = true
-		}
-	}
-
-	return nil
-}
-
 func (c *Conn) writeHeader(cs *ChunkStream) error {
 	// basic header
 	h := uint32(cs.Fmt) << 6
@@ -629,23 +478,3 @@ const (
 	MsgAggregateMessage           RtmpMsgTypeID = 22       //0x16
 )
 
-const (
-	cmdConnect       = "connect"
-	cmdFcpublish     = "FCPublish"
-	cmdReleaseStream = "releaseStream"
-	cmdCreateStream  = "createStream"
-	cmdPublish       = "publish"
-	cmdFCUnpublish   = "FCUnpublish"
-	cmdDeleteStream  = "deleteStream"
-	cmdPlay          = "play"
-)
-
-const (
-	streamBegin uint32 = 0
-	//streamEOF        uint32 = 1
-	//streamDry        uint32 = 2
-	//setBufferLen     uint32 = 3
-	streamIsRecorded uint32 = 4
-	//pingRequest      uint32 = 6
-	//pingResponse     uint32 = 7
-)
--- pkg/rtmp/common.go
+++ pkg/rtmp/common.go
@@ -24,3 +24,24 @@ func genStreamKey(domain, app, stream string) string {
 func genUuid() string {
 	return uuid.NewV4().String()
 }
+
+const (
+	cmdConnect       = "connect"
+	cmdFcpublish     = "FCPublish"
+	cmdReleaseStream = "releaseStream"
+	cmdCreateStream  = "createStream"
+	cmdPublish       = "publish"
+	cmdFCUnpublish   = "FCUnpublish"
+	cmdDeleteStream  = "deleteStream"
+	cmdPlay          = "play"
+)
+
+const (
+	streamBegin uint32 = 0
+	//streamEOF        uint32 = 1
+	//streamDry        uint32 = 2
+	//setBufferLen     uint32 = 3
+	streamIsRecorded uint32 = 4
+	//pingRequest      uint32 = 6
+	//pingResponse     uint32 = 7
+)
